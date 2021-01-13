a7327e8cd830ee99fc7cf021daef0f4092c4be43
강상원 <kevink1113@u.sogang.ac.kr>
2021-01-13 17:50:04 +0900

    minor 내용

--- src/StyleStat.js
+++ src/StyleStat.js
@@ -1,7 +1,11 @@
 function StyleStat() {
     return (
       <div className="StyleStat">
-        Hello22!!
+        <h2>스타일 유형 분석</h2>
+        <h4>스타일 유형에 대한 설명</h4>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec leo neque. Pellentesque sed est mattis, rhoncus elit eu,tortor quis interdum fermentum, magna leo pulvinar arcu, cursus rhoncus nulla turpis quis sem. Aenean lacinia efficitur ex, eget lobortis purus ven
+        </p>
       </div>
     );
   }
--- src/StyleView.js
+++ src/StyleView.js
@@ -3,7 +3,14 @@ import './StyleView.css';
 function StyleView() {
   return (
     <div className="StyleView">
-      Hello!!
+      <h1>당신의 패션 스타일 (유형)은?</h1>
+      <img></img>
+      <h4>강상원</h4>
+      <h3>Urban Style</h3>
+      <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec leo neque. Pellentesque sed"
+      </p>
+      
     </div>
   );
 }
