7f18667c6e6e3a571e687758fa874b951a5445fc
강상원 <kevink1113@u.sogang.ac.kr>
2021-01-14 16:44:49 +0900

    Stat edited

--- src/StyleStat.js
+++ src/StyleStat.js
@@ -1,26 +1,26 @@
 function StyleStat() {
     return (
-      <div className="StyleView">
-      <h1>스타일 유형에 대한 설명</h1>
-      <div className="view_container">
-        
-        <div className="view_text">
-          <h3>스타일 유형</h3>
-          <p>
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Pellentesque nec leo neque. Pellentesque sed"
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec leo neque. 
-            Pellentesque sed est mattis, rhoncus elit eu,tortor quis interdum fermentum, 
-            magna leo pulvinar arcu, cursus rhoncus nulla turpis quis sem. 
-            Aenean lacinia efficitur ex, eget lobortis purus ven
-          </p>
+        <div className="StyleView">
+            <h1>스타일 유형에 대한 설명</h1>
+            <div className="view_container">
+                <div className="view_text">
+                    <h3>스타일 유형</h3>
+                    <p>
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit. Pellentesque nec leo neque. Pellentesque sed"
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Pellentesque nec leo neque. Pellentesque sed est mattis,
+                        rhoncus elit eu,tortor quis interdum fermentum, magna
+                        leo pulvinar arcu, cursus rhoncus nulla turpis quis sem.
+                        Aenean lacinia efficitur ex, eget lobortis purus ven
+                    </p>
+                </div>
+                <img src="./5.png" className="photo"></img>
+            </div>
         </div>
-        <img src="./5.png"></img>
-      </div>
-    </div>
     );
-  }
-  
-  export default StyleStat;
+}
+
+export default StyleStat;
