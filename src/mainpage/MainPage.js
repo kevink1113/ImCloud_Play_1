e74ba917f7326105e41a717a8bc573b8caf46a8f
isdatlhj <isdatlhj@gmail.com>
2021-01-14 17:08:32 +0900

    1312341234124

--- src/mainpage/MainPage.js
+++ src/mainpage/MainPage.js
@@ -4,27 +4,27 @@ import MainPage2 from "./MainPage2";
 import StartButton from "./StartButton";
 
 class MainPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      mainTitle: "Lorem ipsum dolor sit amet",
-      mainContents:
-        " consectetur adipiscing elitPellentesque nec leo neque. Pellentesque sed est mattis, rhoncus elit eu",
-      startB: "시작",
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            mainTitle: "Lorem ipsum dolor sit amet",
+            mainContents:
+                " consectetur adipiscing elitPellentesque nec leo neque. Pellentesque sed est mattis, rhoncus elit eu",
+            startB: "시작",
+        };
+    }
 
-  render() {
-    return (
-      <div class="mainScreen">
-        <MainPage2
-          title={this.state.mainTitle}
-          cont={this.state.mainContents}
-        />
-        <StartButton start={this.state.startB}></StartButton>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div class="mainScreen">
+                <MainPage2
+                    title={this.state.mainTitle}
+                    cont={this.state.mainContents}
+                />
+                <StartButton start={this.state.startB}></StartButton>
+            </div>
+        );
+    }
 }
 
 export default MainPage;
--- src/mainpage/MainPage2.js
+++ src/mainpage/MainPage2.js
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 import "./MainC.css";
 
 class MainPage2 extends Component {
-  render() {
-    return (
-      <div>
-        <h2 id="mainPage_title"> {this.props.title} </h2> <br />
-        <div id="mainPage_cont">{this.props.cont}</div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h2 id="mainPage_title"> {this.props.title} </h2> <br />
+                <div id="mainPage_cont">{this.props.cont}</div>
+            </div>
+        );
+    }
 }
 
 export default MainPage2;
