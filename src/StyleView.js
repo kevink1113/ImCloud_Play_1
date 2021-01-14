0de0aa9545dd8a6d11806cd8c8d777e45f13475c
강상원 <kevink1113@u.sogang.ac.kr>
2021-01-14 10:04:16 +0900

    minor 내용

--- src/StyleView.js
+++ src/StyleView.js
@@ -4,13 +4,20 @@ function StyleView() {
   return (
     <div className="StyleView">
       <h1>당신의 패션 스타일 (유형)은?</h1>
-      <img></img>
-      <h4>강상원</h4>
-      <h3>Urban Style</h3>
-      <p>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec leo neque. Pellentesque sed"
-      </p>
-      
+      <div className="view_container">
+        <img src="./people.png"></img>
+        <div className="view_text">
+          <h4>강상원</h4>
+          <h3>Urban Style</h3>
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec leo neque. Pellentesque sed"
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec leo neque. Pellentesque sed est mattis, rhoncus elit eu,tortor quis interdum fermentum, magna leo pulvinar arcu, cursus rhoncus nulla turpis quis sem. Aenean lacinia efficitur ex, eget lobortis purus ven
+          </p>
+        </div>
+
+      </div>
     </div>
   );
 }
