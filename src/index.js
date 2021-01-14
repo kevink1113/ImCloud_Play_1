ab0e81ebf11a62c7b8a3afb5fa7b8d446812fb06
isdatlhj <isdatlhj@gmail.com>
2021-01-14 10:53:43 +0900

    router 추가
    111

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import StyleView from './StyleView';
-import StyleStat from './StyleStat';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import StyleView from "./StyleView";
+import StyleStat from "./StyleStat";
+import reportWebVitals from "./reportWebVitals";
+import MainPage from "./mainpage/MainPage";
+import { BrowserRouter, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      {/*  시작페이지 */}
+      <Route exact path="/">
+        <MainPage />
+      </Route>
+      {/*  결과 */}
+      <Route path="/result">
+        <StyleView />
+        <StyleStat />
+      </Route>
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <StyleView/>
-    <StyleStat/>
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
